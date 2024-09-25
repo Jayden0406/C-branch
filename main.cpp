7470c67b810360d5290c17798309aaa787c3462f
Jayden0406 <157384715+Jayden0406@users.noreply.github.com>
2024-09-25 14:16:28 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
-// Main Branch for chapter 2 programs (3 programs in this repositorty)
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
-int main() { std::cout << "Hello World!\n"; }
+int main() {
+  // Define variables
+  double gas = 20;
+  double miles_in_town = 23.5;
+  double miles_on_highway = 28.9;
+  double distance_in_town;
+  double distance_on_highway;
+
+  // Calculate distances
+  distance_in_town = gas * miles_in_town;
+  distance_on_highway = gas * miles_on_highway;
+
+  // Display results
+  cout << "Distance in town: " << fixed << setprecision(1) << distance_in_town << " miles" << endl;
+  cout << "Distance on highway: " << fixed << setprecision(1) << distance_on_highway << " miles" << endl;
+
+  return 0;
+}
