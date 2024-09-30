4365bb8eefd577117183448c01e7ede2869d58fd
Jayden0406 <157384715+Jayden0406@users.noreply.github.com>
2024-09-30 14:35:06 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,35 @@
-// Main Branch for chapter 2 programs (3 programs in this repositorty)
 #include <iostream>
+#include <string>
 
-int main() { std::cout << "Hello World!\n"; }
+int main() {
+    int num1, num2;
+    char op;
+
+    std::cout << "Enter two numbers: ";
+    std::cin >> num1 >> num2;
+    std::cout << "Enter an operator (+, -, *, /): ";
+    std::cin >> op;
+
+    switch(op) {
+        case '+':
+            std::cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+        case '-':
+            std::cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+        case '*':
+            std::cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+        case '/':
+            if(num2 == 0) {
+                std::cout << "Error! Division by zero is not allowed.";
+            } else {
+                std::cout << num1 << " / " << num2 << " = " << static_cast<double>(num1) / num2;
+            }
+            break;
+        default:
+            std::cout << "Error! Unsupported operator.";
+    }
+
+    return 0;
+}
