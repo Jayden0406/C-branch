427315cb5dfc08410b39fe69dc5370a46f6015b8
Jayden0406 <157384715+Jayden0406@users.noreply.github.com>
2025-03-06 14:07:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,63 @@
-// Main Branch for chapter 2 programs (3 programs in this repositorty)
 #include <iostream>
 
-int main() { std::cout << "Hello World!\n"; }
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class SinglyLinkedList {
+private:
+    Node* head;
+    void reversePrintHelper(Node* node) {
+        if (node == nullptr) return;
+        reversePrintHelper(node->next);
+        std::cout << node->data << " ";
+    }
+
+public:
+    SinglyLinkedList() : head(nullptr) {}
+
+    void insertAtEnd(int val) {
+        Node* newNode = new Node(val);
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    void displayList() {
+        Node* temp = head;
+        while (temp) {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
+
+    void reversePrint() {
+        reversePrintHelper(head);
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    SinglyLinkedList list;
+    for (int i = 1; i <= 10; i++) {
+        list.insertAtEnd(i);
+    }
+
+    std::cout << "Original List: ";
+    list.displayList();
+
+    std::cout << "Reversed List (using recursion): ";
+    list.reversePrint();
+
+    return 0;
+}
